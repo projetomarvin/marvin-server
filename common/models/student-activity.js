5d10a84110a98b6f252b36d673c1d73f8a2ad666
Dante Nolasco <dnolsacodante@gmail.com>
2018-11-12 16:17:13 -0200

    finish and check codes

--- common/models/student-activity.js
+++ common/models/student-activity.js
@@ -1,6 +1,14 @@
 'use strict';
 const moment = require('moment');
 const axios = require('axios');
+const fs = require('fs');
+const { exec } = require('child_process');
+const AWS = require('aws-sdk');
+const credentials = new AWS.SharedIniFileCredentials({profile: 'cori'});
+AWS.config.region = 'sa-east-1';
+AWS.config.credentials = credentials;
+
+const s3 = new AWS.S3();
 
 module.exports = function(Studentactivity) {
   Studentactivity.beforeRemote('create', function(ctx, data, next) {
@@ -9,38 +17,86 @@ module.exports = function(Studentactivity) {
     next();
   });
 
-  Studentactivity.beforeRemote('finish', async function(ctx, data) {
-    const Activity = Studentactivity.app.models.Activity;
-    const students = Studentactivity.app.models.Student;
-    const stActiity = await Studentactivity.findById(ctx.req.params.id);
-    const stu = await students.findById(stActiity.studentId);
-    const Act = await Activity.findById(stActiity.activityId);
-    Promise.all(
-      Act.exercises.map(r => {
-        const url =
-          `https://api.github.com/repos/${stu.username}/marvin/contents/` +
-          r.file +
-          '?access_token=2551f7fdc3e1bfc7f556b888384a7e7657bdf0e1';
-        axios
-          .get(url)
-          .then(r => console.log(r))
-          .catch(err => {
-            console.log(err);
-            return new Error('not found');
-          });
-      })
-    ).then(r => {
-      console.log(r);
-      return;
-    });
-  });
-
-  Studentactivity.finish = async function(id, cb) {
+  Studentactivity.checkFiles = async function(id) {
     const Activity = Studentactivity.app.models.Activity;
     const students = Studentactivity.app.models.Student;
     const stActiity = await Studentactivity.findById(id);
     const stu = await students.findById(stActiity.studentId);
     const Act = await Activity.findById(stActiity.activityId);
+    try {
+      const data = await Promise.all(
+        Act.exercises.map(async r => {
+          const file = await axios(
+            `https://api.github.com/repos/${stu.username}/marvin/contents/` +
+              r.file +
+              '?access_token=2551f7fdc3e1bfc7f556b888384a7e7657bdf0e1'
+          );
+          console.log(file.data.path);
+          return file.data;
+        })
+      );
+      return data;
+    } catch (e) {
+      const file = e.response.request.path.split('?')[0].split('/');
+      const err = new Error();
+      err.statusCode = 404;
+      err.message = 'file not found ' + file[file.length - 1];
+      throw err;
+    }
+  };
+
+  Studentactivity.remoteMethod('checkFiles', {
+    accepts: {arg: 'id', type: 'string', required: true},
+    returns: {arg: 'events', root: true},
+    http: {path: '/:id/checkFiles', verb: 'get'},
+  });
+
+  Studentactivity.finish = async function(id) {
+    const Activity = Studentactivity.app.models.Activity;
+    const students = Studentactivity.app.models.Student;
+    const stActiity = await Studentactivity.findById(id);
+    const stu = await students.findById(stActiity.studentId);
+    const Act = await Activity.findById(stActiity.activityId);
+    let path = Act.exercises[0].file.split('/');
+    path.splice(-1, 1);
+    path = path.join('/');
+    const folder = '/home/dante/Documents'
+    await fs.mkdirSync(`${folder}/${id}`);
+    await fs.mkdirSync(`${folder}/${id}/${path}`);
+    const files = await Promise.all(
+      Act.exercises.map(async r => {
+        const file = await axios(
+          `https://api.github.com/repos/${stu.username}/marvin/contents/` +
+            r.file +
+            '?access_token=2551f7fdc3e1bfc7f556b888384a7e7657bdf0e1'
+        );
+        fs.writeFileSync(
+          `${folder}/${id}/${r.file}`,
+          file.data.content,
+          'base64',
+          function(err) {
+            console.log('File created', err);
+          }
+        );
+      })
+    );
+    exec(`zip ${folder}/${id}.zip ${folder}/${id}`);
+    exec(`rm -rf ${folder}/${id}`);
+    const file = await fs.readFileSync(`${folder}/${id}.zip`);
+    s3.upload(
+      {
+        Bucket: 'marvin-files',
+        ACL: 'public-read',
+        Key: `${id}.zip`,
+        Body: file,
+      },
+      (err, result) => {
+        fs.unlinkSync(`${folder}/${id}.zip`);
+        stActiity.finishedAt = moment();
+        stActiity.save();
+        // console.log(err, result);
+      }
+    );
   };
 
   Studentactivity.remoteMethod('finish', {
@@ -48,4 +104,22 @@ module.exports = function(Studentactivity) {
     returns: {arg: 'events', root: true},
     http: {path: '/:id/finish', verb: 'put'},
   });
+
+  Studentactivity.afterRemote('finish', async function (ctx, data) {
+    const id = ctx.req.params.id;
+    const courses = Studentactivity.app.models.Course;
+    const students = Studentactivity.app.models.Student;
+    const stActiity = await Studentactivity.findById(id);
+    const stu = await students.findById(stActiity.studentId);
+    const course = await courses.findById(stu.courseId, {include: 'students'});
+    console.log(course.toJSON().students, stu);
+    const sts = course.toJSON().students;
+    let n = Math.floor(Math.random()*sts.length);
+    let corrector;
+    while (sts[n].id === stActiity.studentId) {
+      n = Math.floor(Math.random()*sts.length);
+    };
+    console.log(sts[n]);
+    return {filesURL: `https://s3-sa-east-1.amazonaws.com/marvin-files/${id}.zip`};
+  })
 };
