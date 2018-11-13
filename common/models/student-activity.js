97aa1f962c0ab0b33bae304fc40d18017657599b
Dante Nolasco <dnolsacodante@gmail.com>
2018-11-13 13:47:12 -0200

    fix folder path

--- common/models/student-activity.js
+++ common/models/student-activity.js
@@ -2,7 +2,7 @@
 const moment = require('moment');
 const axios = require('axios');
 const fs = require('fs');
-const { exec, execSync } = require('child_process');
+const {exec, execSync} = require('child_process');
 const AWS = require('aws-sdk');
 const credentials = new AWS.SharedIniFileCredentials({profile: 'cori'});
 AWS.config.region = 'sa-east-1';
@@ -47,9 +47,19 @@ module.exports = function(Studentactivity) {
   };
 
   Studentactivity.remoteMethod('checkFiles', {
-    accepts: {arg: 'id', type: 'string', required: true},
-    returns: {arg: 'events', root: true},
-    http: {path: '/:id/checkFiles', verb: 'get'},
+    accepts: {
+      arg: 'id',
+      type: 'string',
+      required: true,
+    },
+    returns: {
+      arg: 'events',
+      root: true,
+    },
+    http: {
+      path: '/:id/checkFiles',
+      verb: 'get',
+    },
   });
 
   Studentactivity.beforeRemote('finish', async function(ctx, data) {
@@ -57,11 +67,11 @@ module.exports = function(Studentactivity) {
     const stActiity = await Studentactivity.findById(id);
     if (stActiity.finishedAt) {
       const err = new Error();
-      err.message = "atividade já finalizada"
+      err.message = 'atividade já finalizada';
       throw err;
     }
     return;
-  })
+  });
 
   Studentactivity.finish = async function(id) {
     const Activity = Studentactivity.app.models.Activity;
@@ -75,13 +85,11 @@ module.exports = function(Studentactivity) {
     let folder;
     let path = Act.exercises[0].file.split('/');
     path = path[0];
-    fs.stat('/home/ubuntu/activityFiles', function (err, stats) {
-      if (err) {
-        folder = '/home/dante/Documents';
-      } else {
-        folder = '/home/ubuntu/activityFiles';
-      }
-    })
+    if (await fs.existsSync('/home/ubuntu/activityFiles')) {
+      folder = '/home/ubuntu/activityFiles';
+    } else {
+      folder = '/home/dante/Documents';
+    }
     await fs.mkdirSync(`${folder}/${id}`);
     await fs.mkdirSync(`${folder}/${id}/${path}`);
     const files = await Promise.all(
@@ -120,11 +128,11 @@ module.exports = function(Studentactivity) {
         // console.log(err, result);
       }
     );
-    let n = Math.floor(Math.random()*sts.length);
+    let n = Math.floor(Math.random() * sts.length);
     let corrector;
     while (sts[n].id === stActiity.studentId) {
-      n = Math.floor(Math.random()*sts.length);
-    };
+      n = Math.floor(Math.random() * sts.length);
+    }
     console.log(sts[n]);
     stActiity.correctorId = sts[n].id;
     stActiity.finishedAt = moment();
@@ -133,12 +141,21 @@ module.exports = function(Studentactivity) {
       filesURL: `https://s3-sa-east-1.amazonaws.com/marvin-files/${id}.zip`,
       corrector: sts[n],
     };
-
   };
 
   Studentactivity.remoteMethod('finish', {
-    accepts: {arg: 'id', type: 'string', required: true},
-    returns: {arg: 'events', root: true},
-    http: {path: '/:id/finish', verb: 'put'},
+    accepts: {
+      arg: 'id',
+      type: 'string',
+      required: true,
+    },
+    returns: {
+      arg: 'events',
+      root: true,
+    },
+    http: {
+      path: '/:id/finish',
+      verb: 'put',
+    },
   });
 };
