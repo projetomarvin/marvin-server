98355a02a16179033dd700c4245bebc1dd8a3d29
Dante Nolasco <dnolsacodante@gmail.com>
2018-11-21 18:24:02 -0200

    send email after correctrion for feedback

--- common/models/correction.js
+++ common/models/correction.js
@@ -2,34 +2,31 @@
 const moment = require('moment');
 const sgMail = require('@sendgrid/mail');
 
-const sgKey =
-  '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';
+const sgKey = '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';
 sgMail.setApiKey(sgKey);
 
 module.exports = function(Correction) {
-  Correction.beforeRemote('prototype.patchAttributes', function (ctx, data, next) {
+  Correction.beforeRemote('prototype.patchAttributes', function(ctx, data, next) {
     const body = ctx.req.body;
     body.correctedAt = moment().toDate();
-    console.log("done");
     next();
   });
 
-  Correction.afterRemote('prototype.patchAttributes', async function (ctx, data) {
-    const students = Correction.app.models.Student;
-    const Studentactivity = Correction.app.models.Studentactivity;
-    const stu = await Studentactivity.findById(data.StudentactivityId, {
-      include: 'students',
-    });
-    const corr = await students.findById(data.correctorId);
-    console.log(stu.email, corr);
+  Correction.afterRemote('prototype.patchAttributes', async function(ctx, data) {
+    const StudentActivity = Correction.app.models.StudentActivity;
+    const stuAct = await StudentActivity.findById(data.studentActivityId, {include: 'student'});
+    const student = stuAct.toJSON().student;
     const msg = {
-      to: stu.email,
+      to: student.email,
       from: {
         email: 'contato@projetomarvin.com',
-        name: 'Marvin',
+        name: 'Marvin'
       },
       subject: 'Feedback de correção',
-      html: `<p>`
-    }
+      html: `<p>
+      Olá ${student.username}. Sua correção foi encerrada e o último passo para finalizar a fase é o formulário de feedback.<br>
+      <a href= 'https://docs.google.com/forms/d/e/1FAIpQLScVHdY9ApN8qkUzgvurvTjWDBDvRsWMmT3QQUIBVlUUUyLeQA/viewform?entry.805940912=${data.id}' target='_blank'>Clique aqui</a> para responder.`
+    };
+    sgMail.send(msg);
   });
 };
