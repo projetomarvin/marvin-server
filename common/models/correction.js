f7cde8abc8758eba42df18b92dd59b92156b6f37
Dante Nolasco <dnolsacodante@gmail.com>
2018-11-21 15:55:31 -0200

    fixed relations and time info

--- common/models/correction.js
+++ common/models/correction.js
@@ -1,5 +1,20 @@
 'use strict';
+const moment = require('moment');
+const sgMail = require('@sendgrid/mail');
+
+const sgKey =
+  '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';
+sgMail.setApiKey(sgKey);
 
 module.exports = function(Correction) {
+  Correction.beforeRemote('prototype.patchAttributes', function (ctx, data, next) {
+    const body = ctx.req.body;
+    body.correctedAt = moment().toDate();
+    console.log("done");
+    next();
+  });
 
+  Correction.afterRemote('prototype.patchAttributes', async function (ctx, data) {
+
+  });
 };
